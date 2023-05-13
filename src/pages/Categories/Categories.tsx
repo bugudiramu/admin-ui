e3e39b8c367ad8c06ef32c9a4616c66ce16135de
bugudiramu <ramubugudi4@gmail.com>
2023-05-13 19:34:51 +0530

    Categories page UI done

--- src/pages/Categories/Categories.tsx
+++ src/pages/Categories/Categories.tsx
@@ -1,9 +1,14 @@
 import { ColumnsType } from "antd/es/table";
 import AntTable from "../../components/AntTable/AntTable";
-import { Button, Space } from "antd";
+import { Button, Form, Input, Space } from "antd";
 import { ICategoryData } from "./Category.interface";
+import { categories } from "../../constants";
+import { useState } from "react";
+import { RequiredMark } from "antd/es/form/Form";
 
 const Categories = () => {
+  const [form] = Form.useForm();
+
   const columns: ColumnsType<ICategoryData> = [
     {
       title: "Category Name",
@@ -24,14 +29,40 @@ const Categories = () => {
     },
   ];
   const data: ICategoryData[] = [];
-  for (let i = 1; i <= 100; i++) {
+  for (let i = 1; i <= categories.length; i++) {
     data.push({
       key: i,
-      categoryName: "John Brown",
+      categoryName: categories[i],
     });
   }
   return (
     <div>
+      {/* Add new category */}
+      <Form form={form} layout="vertical" requiredMark={true}>
+        <Form.Item
+          label="Create new category"
+          required
+          tooltip="This is a required field"
+          name="createNewCategory"
+        >
+          <Input placeholder="input placeholder" />
+        </Form.Item>
+        <Form.Item>
+          <Space size="small">
+            <Button type="primary" htmlType="submit">
+              Save
+            </Button>
+            <Button
+              onClick={() => {
+                console.log({ form });
+                form.resetFields();
+              }}
+            >
+              Clear
+            </Button>
+          </Space>
+        </Form.Item>
+      </Form>
       <AntTable columns={columns} data={data} />
     </div>
   );
